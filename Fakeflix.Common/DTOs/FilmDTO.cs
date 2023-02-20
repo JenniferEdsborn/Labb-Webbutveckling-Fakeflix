906af060b8a967d237c4536718d9a68a4c32f5af
Jennifer <ki.in@live.se>
2023-02-20 15:30:41 +0100

    film/genre

--- Fakeflix.Common/DTOs/FilmDTO.cs
+++ Fakeflix.Common/DTOs/FilmDTO.cs
@@ -3,15 +3,15 @@
 public class FilmDTO
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public DateTime Released { get; set; }
-    public bool Free { get; set; }
-    public string Description { get; set; }
-    public string FilmUrl { get; set; }
-    public int DirectorId { get; set; }
-    public DirectorDTO Director { get; set; } = new();
-    public List<GenreDTO> Genres { get; set; } = new();
-    public List<SimilarFilmsDTO> SimilarFilms { get; set; } = new(); 
+    public string Title { get; set; } = string.Empty;
+    public DateTime Released { get; set; } = DateTime.Today;
+    public bool Free { get; set; } = false;
+    public string? Description { get; set; } = string.Empty;
+    public string FilmUrl { get; set; } = string.Empty;
+    public int? DirectorId { get; set; }
+    public DirectorDTO? Director { get; set; }
+    public List<GenreDTO>? Genres { get; set; } = new();
+    public List<SimilarFilmsDTO>? SimilarFilms { get; set; } = new(); 
 }
 public class FilmCreateDTO
 {
